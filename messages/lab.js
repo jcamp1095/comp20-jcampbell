ab29c96ee31a6b5fabd5295c6267c1cceb74da80
Joe Campbell <jcamp1095@gmail.com>
2016-03-09 11:51:09 -0500

    first draft at js file

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,20 @@
-// Your JavaScript goes here...
+request = new XMLHttpRequest();
+
+request.open("GET", "data.json", true);
+
+request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+                message = "";
+                raw = request.responseText;
+                data = JSON.parse(raw);
+                elem = document.getElementById("messages");
+                for (i = 0; i < data["id"].length; i++) {
+                        message += "<p>" + data[i]["content"] + data[i]["username"] + "</p>";
+                }
+                elem.innerHTML = message;
+        } else if (request.readyState == 4 && request.status != 200) {
+                document.getElementById("messages").innerHTML = "<p> Failed to load data</p>";
+        }
+};
+
+request.send(null);
